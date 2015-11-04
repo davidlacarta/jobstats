ac886f9310de3750b16c27a75a1bc58467bf733a
davidlacarta <davidlacarta@gmail.com>
2015-11-04 14:59:07 +0000

    add whitenoise

--- jobstats/settings.py
+++ jobstats/settings.py
@@ -106,3 +106,8 @@ STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
