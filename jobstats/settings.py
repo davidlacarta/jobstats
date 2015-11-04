3157d2f1688c02079598c20ba3c16c5da4defbd8
davidlacarta <davidlacarta@gmail.com>
2015-11-04 14:55:38 +0000

    add static root

--- jobstats/settings.py
+++ jobstats/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
