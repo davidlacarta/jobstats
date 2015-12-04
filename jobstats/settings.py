120736cf5524a80a56ad13e96deb4839837aa133
davidlacarta <davidlacarta@gmail.com>
2015-12-04 17:44:49 +0000

    django-admin material design

--- jobstats/settings.py
+++ jobstats/settings.py
@@ -39,6 +39,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = (
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
